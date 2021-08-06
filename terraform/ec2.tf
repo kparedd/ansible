fe1b3e1b16492ce007665a2e75db2698bc64fa39
kparedd <83795719+kparedd@users.noreply.github.com>
2021-08-06 13:38:49 -0400

    ansible

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -3,7 +3,7 @@ resource "aws_spot_instance_request" "cheap_worker" {
   ami                       = "ami-074df373d6bafa625"
   spot_price                = "0.0031"
   instance_type             = "t3.micro"
-  vpc_security_group_ids    = ["sg-0d00b6b80c9e9b60c"]
+  vpc_security_group_ids    = ["sg-03871c9a33425a291"]
   wait_for_fulfillment      = true
   //spot_type                 = "persistent"
   tags                      = {
