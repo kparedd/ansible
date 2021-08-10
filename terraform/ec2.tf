695b3b1cf00333ce9a645249a274a1ce4f458ede
kparedd <83795719+kparedd@users.noreply.github.com>
2021-08-10 11:35:00 -0400

    ansible

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -1,22 +1,18 @@
-resource "aws_spot_instance_request" "cheap_worker" {
+resource "aws_instance" "instance" {
   count                     = local.LENGTH
   ami                       = "ami-074df373d6bafa625"
-  spot_price                = "0.0031"
   instance_type             = "t3.micro"
   vpc_security_group_ids    = ["sg-03871c9a33425a291"]
-  wait_for_fulfillment      = true
-  //spot_type                 = "persistent"
   tags                      = {
     Name                    = element(var.COMPONENTS, count.index)
-  }
 }
 
-resource "aws_ec2_tag" "name-tag" {
-  count                     = local.LENGTH
-  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-  key                       = "Name"
-  value                     = element(var.COMPONENTS, count.index)
-}
+//resource "aws_ec2_tag" "name-tag" {
+ // count                     = local.LENGTH
+//  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
+//  key                       = "Name"
+//  value                     = element(var.COMPONENTS, count.index)
+//}
 
 resource "aws_route53_record" "records" {
   count                     = local.LENGTH
@@ -24,7 +20,7 @@ resource "aws_route53_record" "records" {
   type                      = "A"
   zone_id                   = "Z058717311P9VJRR1I7TD"
   ttl                       = 300
-  records                   = [element(aws_spot_instance_request.cheap_worker.*.private_ip, count.index)]
+  records                   = [element(aws_instance.instance.*.private_ip, count.index)]
 }
 locals {
   LENGTH    = length(var.COMPONENTS)
