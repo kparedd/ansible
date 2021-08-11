602b6d1a89391af5c1c6b29415d9a57df8ea51af
kparedd <83795719+kparedd@users.noreply.github.com>
2021-08-11 10:48:37 -0400

    terraform

--- terraform/ec2.tf
+++ terraform/ec2.tf
@@ -8,12 +8,7 @@ resource "aws_instance" "instances" {
   }
 }
 
-//resource "aws_ec2_tag" "name-tag" {
- // count                     = local.LENGTH
-//  resource_id               = element(aws_spot_instance_request.cheap_worker.*.spot_instance_id, count.index)
-//  key                       = "Name"
-//  value                     = element(var.COMPONENTS, count.index
-//}
+
 
 resource "aws_route53_record" "records" {
   count                     = local.LENGTH
@@ -25,4 +20,11 @@ resource "aws_route53_record" "records" {
 }
 locals {
   LENGTH    = length(var.COMPONENTS)
+}
+
+
+resource "local_file" "inventory-file" {
+  //content     = "[FRONTEND]\n${aws_instance.instances.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.instances.*.private_ip[8]}\n[SHIPPING]\n${aws_instance.instances.*.private_ip[7]}\n[USER]\n${aws_instance.instances.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.instances.*.private_ip[5]}\n[CART]\n${aws_instance.instances.*.private_ip[4]}\n[REDIS]\n${aws_instance.instances.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.instances.*.private_ip[2]}\n[MONGODB]\n${aws_instance.instances.*.private_ip[1]}\n[MYSQL]\n${aws_instance.instances.*.private_ip[0]}\n"
+  content     = "[FRONTEND]\n${aws_instance.instances.*.private_ip[9]}\n[PAYMENT]\n${aws_instance.instances.*.private_ip[8]}\n[SHIPPING]\n${aws_instance.instances.*.private_ip[7]}\n[USER]\n${aws_instance.instances.*.private_ip[6]}\n[CATALOGUE]\n${aws_instance.instances.*.private_ip[5]}\n[CART]\n${aws_instance.instances.*.private_ip[4]}\n[REDIS]\n${aws_instance.instances.*.private_ip[3]}\n[RABBITMQ]\n${aws_instance.instances.*.private_ip[2]}\n[MONGODB]\n${aws_instance.instances.*.private_ip[1]}\n[MYSQL]\n${aws_instance.instances.*.private_ip[0]}\n"
+  filename    = "/tmp/inv-roboshop"
 }
